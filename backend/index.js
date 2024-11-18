071af896e9d4b7902e52ebb3e9b67e4fb519b997
HTM <Mohammadbinaftab26@gmail.com>
2024-11-19 00:01:23 +0530

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
+
 const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
-const FormDataModel = require ('./models/FormData');
-
+const FormDataModel = require('./models/FormData');
 
 const app = express();
 app.use(express.json());
@@ -10,46 +10,44 @@ app.use(cors());
 
 mongoose.connect('mongodb+srv://root:root@google-docs-clone.8to5ii1.mongodb.net/?retryWrites=true&w=majority&appName=Google-Docs-Clone');
 
-app.post('/register', (req, res)=>{
-    // To post / insert data into database
-
-    const {email, password} = req.body;
-    FormDataModel.findOne({email: email})
-    .then(user => {
-        if(user){
-            res.json("Already registered")
-        }
-        else{
-            FormDataModel.create(req.body)
-            .then(log_reg_form => res.json(log_reg_form))
-            .catch(err => res.json(err))
-        }
-    })
-    
-})
-
-app.post('/login', (req, res)=>{
-    // To find record from the database
-    const {email, password} = req.body;
-    FormDataModel.findOne({email: email})
-    .then(user => {
-        if(user){
-            // If user found then these 2 cases
-            if(user.password === password) {
-                res.json("Success");
+app.post('/register', (req, res) => {
+    const { email, password } = req.body;
+    FormDataModel.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                res.json("Already registered");
+            } else {
+                FormDataModel.create(req.body)
+                    .then(log_reg_form => res.json(log_reg_form))
+                    .catch(err => res.json(err));
             }
-            else{
-                res.json("Wrong password");
-            }
-        }
-        // If user not found then 
-        else{
-            res.json("No records found! ");
-        }
-    })
-})
-
-app.listen(3001, () => {
-    console.log("Server listining on http://127.0.0.1:3001");
-
+        });
 });
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    FormDataModel.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                if (user.password === password) {
+                    res.json("Success");
+                } else {
+                    res.json("Wrong password");
+                }
+            } else {
+                res.json("No records found!");
+            }
+        });
+});
+
+// Listen on a port, use process.env.PORT for deployment
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
+
+// Allow requests from your frontend URL
+app.use(cors({
+    origin: 'https://test-phi-green-37.vercel.app',
+    methods: ['GET', 'POST'],
+}));
